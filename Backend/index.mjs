e0095faac36f6d81c5a8e1e28f1c1445dac5bd6b
zach7815 <53745970+zach7815@users.noreply.github.com>
2022-10-17 21:57:28 +0100

    moved setHeader out of conditional

--- Backend/index.mjs
+++ Backend/index.mjs
@@ -15,16 +15,18 @@ app.use(express.json());
 
 
 app.use(function (req, res, next) {
-    const allowedOrigins= ['https://weather-dashboard-backend.onrender.com/', "https://weather-dashboard-backend.onrender.com/api/search"]
+    const allowedOrigins= ['https://weather-dashboard-backend.onrender.com/',
+     "https://weather-dashboard-backend.onrender.com/api/search",
+
+    ]
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)){
         res.setHeader('Access-Control-Allow-Origin', origin);
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    }
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
-    }
-
 
 
     });
