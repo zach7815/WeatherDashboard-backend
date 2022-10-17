fb179be0969a1f4128c437037da17587a363e71d
zach7815 <mcfeegles@gmail.com>
2022-10-17 16:02:09 +0100

    Moved cors to own dir

--- Backend/index.mjs
+++ Backend/index.mjs
@@ -3,19 +3,18 @@ import * as dotenv from 'dotenv';
 import nodeFetch from 'node-fetch';
 import { createApi } from 'unsplash-js';
 import openCage from 'opencage-api-client';
-import cors from "cors";
+import cors from "./cors.js";
+
 dotenv.config();
 const app = express();
+app.options("*", cors);
+app.use(cors);
+
 app.use(express.static('public'));
 app.use(express.json({limit:'1mb'}));
 app.use(express.json());
 
-const corsOptions = {
-    origin: 'https://weather-dashboard.onrender.com/',
-    optionsSuccessStatus: 200, // For legacy browser support
-}
 
-app.use(cors(corsOptions));
 
 app.use(function(req, res, next){
     res.setHeader('Access-Control-Allow-Origin', 'https://weather-dashboard.onrender.com/');
@@ -176,9 +175,9 @@ const randomNumber = (max)=>{
 
 
 // Post requests
-app.post("/api/search", cors(corsOptions),async (req,res)=>{
+app.post("/api/search", async (req,res)=>{
 const location= req.body;
-try{ 
+try{
 const photoInfo= await getImage(location);
 const weather= await getWeather("weather",undefined, undefined, location.city);
 const forecast = await getForecast("forecast",undefined, undefined, location.city);
@@ -192,7 +191,7 @@ res.end()
 
 })
 
-app.post("/api/unsplashImages", cors(corsOptions) ,async (req,res)=>{
+app.post("/api/unsplashImages" ,async (req,res)=>{
 
     let    lat=req.body.lat;
     let  lng=req.body.lng;
@@ -213,7 +212,7 @@ catch(error){
 
 })
 
-app.post("/api/currentWeather",cors(corsOptions),async (req,res)=>{
+app.post("/api/currentWeather", async (req,res)=>{
  const lat=req.body.lat;
  const lng=req.body.lng;
     try{
@@ -226,7 +225,7 @@ res.end()
     }
 })
 
-app.post("/api/fiveDayForecast",cors(corsOptions),async(req,res)=>{
+app.post("/api/fiveDayForecast", async(req,res)=>{
 let lat=req.body.lat;
 let lng=req.body.lng;
     try{
