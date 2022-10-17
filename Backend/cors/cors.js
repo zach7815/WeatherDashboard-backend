4b5a75e92276d3ba16518ce05d721ad81adedcf3
zach7815 <53745970+zach7815@users.noreply.github.com>
2022-10-17 21:29:39 +0100

    deleted cors, allowed api/search origin

--- Backend/cors/cors.js
+++
@@ -1,7 +0,0 @@
-import cors from "cors";
-
-const corsOptions ={
-    origin:"https://weather-dashboard.onrender.com/",
-    optionSuccessStatus:200,
-}
- export default cors(corsOptions);
--- Backend/index.mjs
+++ Backend/index.mjs
@@ -15,7 +15,12 @@ app.use(express.json());
 
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://weather-dashboard-backend.onrender.com/');
+    const allowedOrigins= ['https://weather-dashboard-backend.onrender.com/', "https://weather-dashboard-backend.onrender.com/api/search"]
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)){
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
