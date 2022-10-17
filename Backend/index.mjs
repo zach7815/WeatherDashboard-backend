663f3d236fa540ccf8299c5e7c296b5f04e3aea1
zach7815 <53745970+zach7815@users.noreply.github.com>
2022-10-17 21:36:25 +0100

    amended setheaders code

--- Backend/index.mjs
+++ Backend/index.mjs
@@ -19,12 +19,14 @@ app.use(function (req, res, next) {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)){
         res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
+    }
+
+
+
     });
 
 
