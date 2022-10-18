08e51528f3bf87bbaaaf0b49ddc9bd8bbfe2171f
zach7815 <53745970+zach7815@users.noreply.github.com>
2022-10-18 13:02:01 +0100

    changed Allowed Headers

--- Backend/index.mjs
+++ Backend/index.mjs
@@ -11,7 +11,7 @@ const app = express();
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', "https://weather-dashboard-backend.onrender.com" );
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
     });
